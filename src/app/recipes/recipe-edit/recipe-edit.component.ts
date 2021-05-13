86d521e367c4d4fcb551601b64862f3e1f5220b9
stephen harris <subliminalvisions@gmail.com>
2021-05-12 18:01:12 -0600

    wiring up ingredients array to Recipe Edit Form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -25,15 +25,43 @@ export class RecipeEditComponent implements OnInit {
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
+        this.initForm();
       }
     );
+  }
+  initForm() {
+    let recipeName = '';
+    let recipeImagePath = '';
+    let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
+
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipebyID(this.id);
+      console.log(recipe);
+      recipeName = recipe.name;
+      recipeImagePath = recipe.imagePath;
+      recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount),
+            })
+          )
+        }
+        // array.forEach(element => {
+
+        // });
+      }
+    }
     this.recipeForm = new FormGroup({
-      'name': new FormControl(null, [Validators.required]),
-      'description': new FormControl(null, [Validators.required]),
-      'imagePath': new FormControl(null, [Validators.required]),
-      'ingredients': new FormArray([])
+      'name': new FormControl(recipeName, [Validators.required]),
+      'description': new FormControl(recipeDescription , [Validators.required]),
+      'imagePath': new FormControl(recipeImagePath, [Validators.required]),
+      'ingredients': recipeIngredients
     });
-    // how to link up form with Recipe???
+    // how to link up form with Recipe in Edit Mode???
   }
   onAddIngredient() {
     const control = new FormControl(null, Validators.required);
@@ -42,13 +70,19 @@ export class RecipeEditComponent implements OnInit {
   onSubmitRecipe() {
     console.log(this.recipeForm);
     console.log(this.recipeForm.value.recipeName);
-    this.recipe = {
-      name: this.recipeForm.value.recipeName,
-      description: this.recipeForm.value.recipeName,
-      ingredients: this.recipeForm.value.ingredients,
-      imagePath: 'https://img.sndimg.com/food/image/upload/q_92,fl_progressive,w_1200,c_scale/v1/img/recipes/30/32/45/tKqC3hipQA2MRyTEsneh_oven-bbq-ribs-02757.jpg',
-    };
-    this.recipeService.addRecipe(this.recipe);
+    // this.recipe = {
+    //   name: this.recipeForm.value.recipeName,
+    //   description: this.recipeForm.value.recipeName,
+    //   ingredients: this.recipeForm.value.ingredients,
+    //   imagePath: 'https://img.sndimg.com/food/image/upload/q_92,fl_progressive,w_1200,c_scale/v1/img/recipes/30/32/45/tKqC3hipQA2MRyTEsneh_oven-bbq-ribs-02757.jpg',
+    // };
+    // this.recipeService.addRecipe(this.recipe);
+  }
+
+  get controls() { // a getter!
+    const ingredients = (<FormArray>this.recipeForm.get('ingredients')).controls;
+    // console.log(ingredients);
+    return ingredients;
   }
 
 }
