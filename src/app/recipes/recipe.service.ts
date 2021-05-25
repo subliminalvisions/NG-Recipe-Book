ab8b0dd0e70ce5f65f276e7933c1203954c9f7ec
stephen harris <subliminalvisions@gmail.com>
2021-05-24 22:04:50 -0600

    simplify fetch from db logic

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -35,6 +35,10 @@ export class RecipeService {
 
     constructor(private slService: ShoppingListService) {}
 
+    setRecipes(recipes: Recipe[]): void {
+      this.recipes = recipes;
+      this.recipesChanged.next(this.recipes.slice());
+    }
     getRecipes(): Recipe[] {
         // reminder: slice returns a copy
         return this.recipes.slice();
